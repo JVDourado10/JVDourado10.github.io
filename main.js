9cd1837f2fd6907b0f68775e2a9b76a1785e8c22
JVDourado10 <jovneiva@gmail.com>
2023-05-23 12:19:31 -0300

    add: Novos projetos e funcionalidade: mostrar mais

---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  $(".p2 .mais").click(function () {
+    if ($(".experiencia-projeto.p2 p").css("display") == "none") {
+      $(".experiencia-projeto.p2 p").css({ display: "flex" });
+      $(".p2 .projeto-habilidades").css({ display: "flex" });
+      $(".p2 .projeto-habilidades p").css({ display: "flex" });
+      $(".p2 .mais").html("Mostrar Menos");
+    } else {
+      $(".experiencia-projeto.p2 p").css({ display: "none" });
+      $(".p2 .projeto-habilidades").css({ display: "none" });
+      $(".p2 .projeto-habilidades p").css({ display: "none" });
+      $(".p2 .mais").html("Mostrar Mais");
+    }
+  });
+  $(".p3 .mais").click(function () {
+    if ($(".experiencia-projeto.p3 p").css("display") == "none") {
+      $(".experiencia-projeto.p3 p").css({ display: "flex" });
+      $(".p3 .projeto-habilidades").css({ display: "flex" });
+      $(".p3 .projeto-habilidades p").css({ display: "flex" });
+      $(".p3 .mais").html("Mostrar Menos");
+    } else {
+      $(".experiencia-projeto.p3 p").css({ display: "none" });
+      $(".p3 .projeto-habilidades").css({ display: "none" });
+      $(".p3 .projeto-habilidades p").css({ display: "none" });
+      $(".p3 .mais").html("Mostrar Mais");
+    }
+  });
+  $(".p4 .mais").click(function () {
+    if ($(".experiencia-projeto.p4 p").css("display") == "none") {
+      $(".experiencia-projeto.p4 p").css({ display: "flex" });
+      $(".p4 .projeto-habilidades").css({ display: "flex" });
+      $(".p4 .projeto-habilidades p").css({ display: "flex" });
+      $(".p4 .mais").html("Mostrar Menos");
+    } else {
+      $(".experiencia-projeto.p4 p").css({ display: "none" });
+      $(".p4 .projeto-habilidades").css({ display: "none" });
+      $(".p4 .projeto-habilidades p").css({ display: "none" });
+      $(".p4 .mais").html("Mostrar Mais");
+    }
+  });
+  $(".p5 .mais").click(function () {
+    if ($(".experiencia-projeto.p5 p").css("display") == "none") {
+      $(".experiencia-projeto.p5 p").css({ display: "flex" });
+      $(".p5 .projeto-habilidades").css({ display: "flex" });
+      $(".p5 .projeto-habilidades p").css({ display: "flex" });
+      $(".p5 .mais").html("Mostrar Menos");
+    } else {
+      $(".experiencia-projeto.p5 p").css({ display: "none" });
+      $(".p5 .projeto-habilidades").css({ display: "none" });
+      $(".p5 .projeto-habilidades p").css({ display: "none" });
+      $(".p5 .mais").html("Mostrar Mais");
+    }
+  });
+  $(".p1 .mais").click(function () {
+    if ($(".experiencia-projeto.p1 p").css("display") == "none") {
+      $(".experiencia-projeto.p1 p").css({ display: "flex" });
+      $(".p1 .projeto-habilidades").css({ display: "flex" });
+      $(".p1 .projeto-habilidades p").css({ display: "flex" });
+      $(".p1 .mais").html("Mostrar Menos");
+    } else {
+      $(".experiencia-projeto.p1 p").css({ display: "none" });
+      $(".p1 .projeto-habilidades").css({ display: "none" });
+      $(".p1 .projeto-habilidades p").css({ display: "none" });
+      $(".p1 .mais").html("Mostrar Mais");
+    }
+  });
+});
